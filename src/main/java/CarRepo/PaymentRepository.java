4d794e83d251b2fe33f389207175f265cbacde97
Yerrappa Surendra <ysm69242@gmail.com>
2026-08-17 11:14:29 +0530

    fix: PaymentRepository findByBookingUserId - use @Query to resolve property path
    Spring Data JPA parsed findByBookingUserId as booking.user.id but
    UserTable has userId not id. Using @Query with JPQL to fix this.

--- src/main/java/CarRepo/PaymentRepository.java
+++ src/main/java/CarRepo/PaymentRepository.java
@@ -3,6 +3,8 @@ package CarRepo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import CarEntity.PaymentTable;
@@ -13,7 +15,8 @@ public interface PaymentRepository extends JpaRepository<PaymentTable, Integer> 
 
     PaymentTable findByBookingBookingId(int bookingId);
 
-    List<PaymentTable> findByBookingUserId(int userId);
+    @Query("SELECT p FROM PaymentTable p WHERE p.booking.user.userId = :userId")
+    List<PaymentTable> findByBookingUserId(@Param("userId") int userId);
 
     List<PaymentTable> findByPaymentStatus(payment_status paymentStatus);
 }
